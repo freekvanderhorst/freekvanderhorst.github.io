fce822e37252f1e469dd24dd48b589ea237b949a
freekvanderhorst <freek.vanderhorst@axendo.nl>
2023-01-16 14:18:07 +0100

    menu updates

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,14 +1,16 @@
-const dropdown = document.getElementsByClassName('dropdown')[0]
-const trigger = document.getElementById('menu--trigger')
-const closeTrigger = document.getElementById('close--dropdown')
-
+const dropdown = document.getElementsByClassName('dropdown')[0];
+const trigger = document.getElementById('menu--trigger');
+const closeTrigger = document.getElementById('close--dropdown');
+const body = document.getElementsByTagName('body')[0]
 
 trigger.addEventListener('click', function(){
-    dropdown.classList.add('--active')
-    trigger.classList.add('--active')
+    dropdown.classList.add('--active');
+    trigger.classList.add('--active');
+    body.classList.add('--menu-open');
 });
 
 closeTrigger.addEventListener('click', function(){
     dropdown.classList.remove('--active')
     trigger.classList.remove('--active')
-})
+    body.classList.remove('--menu-open');
+});
